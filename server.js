78acc01cad42e06d4010afa6d9a01f0362021310
Ciunilius <ciuniliusmasih@gmail.com>
2025-03-30 16:50:32 +0500

    first commit

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");;
 const fs = require("fs");
-let port = 2000;
+require('dotenv').config({path: ".env"});
+let port = process.env.PORT;
 let staticPath = path.join(__dirname, './static');
+let viewPath = path.join(__dirname, './views');
 app.use('/static', express.static(staticPath));
-app.set('view engine', 'html')
+app.set('view engine', 'html');
+app.set('views', viewPath);
 app.engine('html', (path, options, cb)=>{
     fs.readFile(path, (err,data)=>{
         if(err) return cb(err);
